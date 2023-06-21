f23aa3bc54c1e79f9d1b020184c5bd3ddb460513
Angel-ISO <angelgabrielorteg@gmail.com>
2023-06-21 09:11:52 -0500

    ff

---
+++ loopDay_c#/Program.cs
@@ -0,0 +1,104 @@
+﻿
+// namespace MyApplication
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       int i = 0;
+//       while (i < 5) 
+//       {
+//         Console.WriteLine(i);
+//         i++;
+//       }
+//     }
+//   }
+// } 
+
+// insternal class Program
+// {
+//   private static void Main(string[] args)
+//   {
+
+//     int[] edades = {12,33,34,21};
+//     foreach (int edad in edades){
+//       Console.Write(edades.length $"{edad}, ");
+//       }
+//    }
+
+// }
+
+
+// using System.Collections;
+
+
+// internal class program
+// {
+//      private static void Main(string[] args)
+//     { 
+//         ArrayList myAL = new ArrayList();
+//         myAL.Add("the");
+//         myAL.Add("quick");
+//         myAL.Add("brown");
+//         myAL.Add("fox");
+//         foreach(object n in myAL){
+//             Console.WriteLine(n.ToString());
+//         }
+
+//     }
+
+// }
+
+
+// using system;
+// using System.Collections;
+
+// internal class Program
+// {
+// private static void Main(string[] args)
+//    {
+//    ArrayList carnivoros = new ArrayList();
+//    ArrayList acuaticos = new ArrayList();
+//    ArrayList animales = new ArrayList();
+
+
+//     carnivoros.Add("leon");
+//     carnivoros.Add("tigre");
+//     carnivoros.Add("pantera");
+//     carnivoros.Add("leopardo");
+//     acuaticos.Add("tiburon");
+//     acuaticos.Add("delfin");
+//     acuaticos.Add("ballena");
+//     acuaticos.Add("pulpo"); 
+
+//    animales.AddRange(carnivoros);
+//    foreach (object n in animales){
+
+//       Console.WriteLine(n.ToString());
+//    }
+//   Console.WriteLine("Presiona enter para continuar");
+//   Console.readLine();
+  
+
+//   animales.Clear();
+//   Console.WriteLine("=================================");
+
+
+//   animales.AddRange(acuaticos);
+//   foreach(object n in animales){
+      
+//       Console.WriteLine(n.ToString());
+//          }
+
+
+// }
+ 
+// }
+
+
+
+
+// se requiere construir un programa que permita ingresar N cantidad de productos. El programa debe permitir guardar la informacion: nombre,precio y cantidad.  
+//El programa debe permitir registrar los productos buscar un determinado producto  y mostrar el precio de cantidad
+//mostrar todos los productos con su valor y cantidad
+
